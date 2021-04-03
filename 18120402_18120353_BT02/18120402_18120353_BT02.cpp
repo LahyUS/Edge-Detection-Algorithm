16fcfc1a2e24bce14bf5fdc44387b300bf9abac4
LahyUS <lahyus7399@gmail.com>
2021-04-04 00:20:17 +0700

    Syncronize and Test

--- 18120402_18120353_BT02/18120402_18120353_BT02.cpp
+++ 18120402_18120353_BT02/18120402_18120353_BT02.cpp
@@ -1,90 +1,33 @@
-#include <iostream>
 #include "opencv2/opencv.hpp "
 #include "opencv2/highgui/highgui.hpp"
 #include <string>
 #include "Edge_Detector.h"
+#include "Convolutioner.h"
 
 using namespace cv;
 using namespace std;
 
 int main(int argc, char** argv)
 {
-	//Mat grayscale_image = imread("C:\\Users\\pc\\Desktop\\Temparory\\Lenna.png", IMREAD_GRAYSCALE); // (2)
-	//Mat dstimg;
-	//ColorTransformer trans;
-	//trans.ConvertGrayscaletoRGB(grayscale_image, dstimg);
-	//trans.ConvertRGBtoGrayscale(grayscale_image, dstimg);
+	Mat src_img = imread("C:\\Users\\pc\\Desktop\\Temparory\\Lenna.png", IMREAD_COLOR); // (2)
+	Mat dst_img;
+	dst_img.create(src_img.rows, src_img.cols, src_img.type());
 
-	// Check number of argument is correct
-	if (argc < 2)
-	{
-		cout << "Photoshop program" << endl;
-		return -1;
-	}
-
-	// Intialization
-	Mat color_image, dst_img, grayscale_image;
-	int code = atoi(argv[2]);
-	int optional = atoi(argv[3]);
 	Edge_Detector ed;
-
-	// Load color image
-	color_image = imread(argv[1], IMREAD_COLOR);
-	if (!color_image.data)
-	{
-		cout << "Cannot open image" << std::endl;
-		return -1;
-	}
-
-	// Edge detection using Sobel kernel
-	if (code == 0)
-	{
-		// Show destination image
-		namedWindow("Display window", WINDOW_AUTOSIZE);
-		imshow("Display window", color_image);
-		waitKey(0);
-
-		ed.detectBySobel(color_image, dst_img);
-	}
-
-	// Edge detection using Prewitt kernel
-	else if (code == 1)
-	{
-		// Show destination image
-		namedWindow("Display window", WINDOW_AUTOSIZE);
-		imshow("Display window", grayscale_image);
-		waitKey(0);
-
-		ed.detectByPrewitt(grayscale_image, dst_img);
-	}
-
-	// Edge detection using Laplace kernel
-	else if (code == 2)
-	{
-		// Show destination image
-		namedWindow("Display window", WINDOW_AUTOSIZE);
-		imshow("Display window", color_image);
-		waitKey(0);
-
-		short brightness = atoi(argv[3]);
-		ed.detectByLaplace(color_image, dst_img);
-	}
-
-	// Edge detection using Cany method
-	else if (code == 3)
-	{
-		// Show destination image
-		namedWindow("Display window", WINDOW_AUTOSIZE);
-		imshow("Display window", color_image);
-		waitKey(0);
-
-		float contrast = atof(argv[3]);
-		ed.detectByCany(color_image, dst_img);
-	}
+	int result = ed.detectByLaplace(src_img, dst_img);
 
 	// Show destination image
-	namedWindow("Display window", WINDOW_AUTOSIZE);
-	imshow("Display window", dst_img);
-	waitKey(0);
+	if (result == 1)
+	{
+		imshow("Source Image", src_img);
+		imshow("Destination Image", dst_img);
+
+		waitKey(0);
+	}
+	else
+	{
+		cout << "Cannot open image\n";
+	}
+
 	return 0;
 }
--- 18120402_18120353_BT02/Edge_Detector.cpp
+++ 18120402_18120353_BT02/Edge_Detector.cpp
@@ -1,45 +1,45 @@
 #include "Edge_Detector.h"
 #include "Convolutioner.h"
 
-int Edge_Detector::detectBySobel(Mat src, Mat dst)
+int Edge_Detector::detectBySobel(Mat sourceImage, Mat destinationImage)
 {
-	Convolutioner convX, convY;	// Declare a convolutioner
-
+	Convolutioner conv;
 	vector<float> kernel_x;
-	kernel_x = { 1, 0, -1, 2, 0, -2, 1, 0, -1 };
+	kernel_x = { 1, 0, -1,2,0,-2,1,0,-1 };
 	for (int i = 0; i < 9; i++) {
-		kernel_x[i] = kernel_x[i] * 1.0 / 9;
+		kernel_x[i] = kernel_x[i] * 1.0 / 1;
 	}
-	convX.SetKernel(kernel_x, 3, 3);
+	conv.SetKernel(kernel_x, 3, 3);
 
-	int width = src.cols, height = src.rows;
+
+	int width = sourceImage.cols, height = sourceImage.rows;
 	Mat dstFx;
-	dstFx.create(height, width, src.type());
-	convX.DoConvolution(src, dstFx);
+	dstFx.create(height, width, sourceImage.type());
+	conv.DoConvolution(sourceImage, dstFx);
 
 	vector<float> kernel_y;
 	kernel_y = { -1,-2,-1,0,0,0,1,2,1 };
 	for (int i = 0; i < 9; i++) {
-		kernel_y[i] = kernel_y[i] * 1.0 / 9;
+		kernel_y[i] = kernel_y[i] * 1.0 / 1;
 	}
-	convY.SetKernel(kernel_y, 3, 3);
+	conv.SetKernel(kernel_y, 3, 3);
 
 	Mat dstFy;
-	dstFy.create(height, width, src.type());
-	convY.DoConvolution(src, dstFy);
+	dstFy.create(height, width, sourceImage.type());
+	conv.DoConvolution(sourceImage, dstFy);
 
-	int nChannels = src.channels();
+	int nChannels = sourceImage.channels();
 	int size_row_raw = width * nChannels;
-	int rem = size_row_raw % 9;
+	int rem = size_row_raw % 4;
 	int widthStep = (rem == 0) ? size_row_raw : size_row_raw + rem;
 
 	uchar* pxData = dstFx.data;
 	uchar* pyData = dstFy.data;
-	uchar* pData = dst.data;
+	uchar* pData = destinationImage.data;
 
 	uchar* pxRow = dstFx.data;
 	uchar* pyRow = dstFy.data;
-	uchar* pRow = dst.data;
+	uchar* pRow = destinationImage.data;
 
 	float sum;
 	for (int i = 0; i < height; i++, pxData += widthStep, pyData += widthStep, pData += widthStep)
@@ -60,9 +60,58 @@ int Edge_Detector::detectByPrewitt(Mat src, Mat dst)
 	return 1;
 }
 
-int Edge_Detector::detectByLaplace(Mat src, Mat des)
+int Edge_Detector::detectByLaplace(Mat sourceImage, Mat destinationImage)
 {
-	return 1;
+	{
+		Convolutioner conv;
+		vector<float> kernel_x;
+		kernel_x = { 0,-1,0,-1,4,-1,0,-1,0 };
+		conv.SetKernel(kernel_x, 3, 3);
+		for (int i = 0; i < 9; i++) {
+			kernel_x[i] = kernel_x[i] * 1.0 / 4;
+		}
+
+		int width = sourceImage.cols, height = sourceImage.rows;
+		Mat dstFx;
+		dstFx.create(height, width, sourceImage.type());
+		conv.DoConvolution(sourceImage, dstFx);
+
+		vector<float> kernel_y;
+		kernel_y = { -1,-1,-1,-1,8,-1,-1,-1,-1 };
+		conv.SetKernel(kernel_y, 3, 3);
+		for (int i = 0; i < 9; i++) {
+			kernel_y[i] = kernel_y[i] * 1.0 / 4;
+		}
+		Mat dstFy;
+		dstFy.create(height, width, sourceImage.type());
+		conv.DoConvolution(sourceImage, dstFy);
+
+		int nChannels = sourceImage.channels();
+		int size_row_raw = width * nChannels;
+		int rem = size_row_raw % 4;
+		int widthStep = (rem == 0) ? size_row_raw : size_row_raw + rem;
+
+		uchar* pxData = dstFx.data;
+		uchar* pyData = dstFy.data;
+		uchar* pData = destinationImage.data;
+
+		uchar* pxRow = dstFx.data;
+		uchar* pyRow = dstFy.data;
+		uchar* pRow = destinationImage.data;
+
+		float sum;
+		for (int i = 0; i < height; i++, pxData += widthStep, pyData += widthStep, pData += widthStep)
+		{
+			pRow = pData;
+			pxRow = pxData;
+			pyRow = pyData;
+			for (int j = 0; j < width * nChannels; j++, pxRow += 1, pyRow += 1, pRow += 1) {
+				sum = sqrt(pxRow[0] * pxRow[0] * 1.0f + pyRow[0] * pyRow[0] * 1.0f);
+				pRow[0] = saturate_cast<uchar>(sum);
+			}
+		}
+		return 1;
+	}
 }
 
 int Edge_Detector::detectByCany(Mat sourceImage, Mat destinationImage)
